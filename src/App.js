8426f76aba7d4e0c4a198fec0ae9c4ec58d161b9
Antônio Sousa <developerantoniosousa@gmail.com>
2020-01-21 13:56:06 -0300

    feat: example with Skeleton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Skeleton from '@material-ui/lab/Skeleton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Skeleton variant="rect" width={210} height={118} />
   );
 }
 
